e69202aed4d684a90ae193a364e09f3c77bcc88e
can <can@bfydigital.com>
2022-05-08 11:29:49 +0300

    first commit

---
+++ Function1.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureFuncToCosmosDB
+{
+    public static class Function1
+    {
+        private static string constr = Environment.GetEnvironmentVariable("ConnectionString");
+
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName:"my-learn-database", collectionName:"my-learn-container", 
+            ConnectionStringSetting = "Connectionstring")]IAsyncCollector<dynamic> documentsOut,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                await documentsOut.AddAsync(new { id = System.Guid.NewGuid().ToString(), name = name });
+            }
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Learn Azure Functions and Cosmos Db
+
+Created: 06.06.2021
